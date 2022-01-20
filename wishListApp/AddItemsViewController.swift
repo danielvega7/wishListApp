d2c112a8bfeba4b5c99aa089833c37490efd0c5b
Daniel Vega <daniel.vega23@d155.org>
2022-01-20 09:22:19 -0600

    story board changes

---
+++ wishListApp/AddItemsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddItemsViewController.swift
+//  wishListApp
+//
+//  Created by DANIEL VEGA on 1/20/22.
+//
+
+import UIKit
+
+class AddItemsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
