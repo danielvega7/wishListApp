e3e60d4223a48e8d0b37c808760d374c3910b05a
Avnish K <avnish.khandeshi24@d155.org>
2022-01-26 09:50:22 -0600

    changed stuff

--- wishListApp/ViewController.swift
+++ wishListApp/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet weak var createAGroupButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        createAGroupButton.backgroundColor = UIColor(patternImage: UIImage(named: "createAGroupButtonBackground"))
     }
 
     
