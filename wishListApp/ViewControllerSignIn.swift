3f15bca4aa799bd93fd179f669447305c58f67ff
Daniel Vega <drvega7@icloud.com>
2022-01-31 07:17:36 -0600

    added sign in view controller

---
+++ wishListApp/ViewControllerSignIn.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerSignIn.swift
+//  wishListApp
+//
+//  Created by Daniel Vega on 1/29/22.
+//
+
+import UIKit
+
+class ViewControllerSignIn: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
