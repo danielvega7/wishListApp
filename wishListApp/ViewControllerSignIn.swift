08b2030f0c3de170ee93219325b7cd2385597bc2
Avnish K <avnish.khandeshi24@d155.org>
2022-02-08 09:53:05 -0600

    removed segmented control

--- wishListApp/ViewControllerSignIn.swift
+++ wishListApp/ViewControllerSignIn.swift
@@ -9,42 +9,27 @@ import UIKit
 
 class ViewControllerSignIn: UIViewController {
     
+    @IBOutlet weak var segmentedControlStackView: UIStackView!
     
-    @IBOutlet weak var signInOrSignUpSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var firstButton: UIButton!
+    
+    @IBOutlet weak var secondButton: UIButton!
+    
+    
+    @IBOutlet weak var firstImageView: UIImageView!
+    
+    @IBOutlet weak var secondImageView: UIImageView!
+    
     
     
     override func viewWillAppear(_ animated: Bool) {
- 
-        signInOrSignUpSegmentedControl.setTitleTextAttributes([
-            NSAttributedString.Key.font : UIFont(name: "DINCondensed-Bold", size: 18),
-            NSAttributedString.Key.foregroundColor: UIColor.lightGray
-            ], for: .normal)
-        signInOrSignUpSegmentedControl.setTitleTextAttributes([
-            NSAttributedString.Key.font : UIFont(name: "DINCondensed-Bold", size: 18),
-            NSAttributedString.Key.foregroundColor: UIColor.orange
-            ], for: .selected)
         
-        let buttonBar = UIView()
-        // This needs to be false since we are using auto layout constraints
-        buttonBar.translatesAutoresizingMaskIntoConstraints = false
-        buttonBar.backgroundColor = UIColor.orange
-        // Below view.addSubview(segmentedControl)
-        view.addSubview(buttonBar)
-        // Constrain the top of the button bar to the bottom of the segmented control
-        buttonBar.topAnchor.constraint(equalTo: signInOrSignUpSegmentedControl.bottomAnchor).isActive = true
-        buttonBar.heightAnchor.constraint(equalToConstant: 5).isActive = true
-        // Constrain the button bar to the left side of the segmented control
-        buttonBar.leftAnchor.constraint(equalTo: signInOrSignUpSegmentedControl.leftAnchor).isActive = true
-        // Constrain the button bar to the width of the segmented control divided by the number of segments
-        buttonBar.widthAnchor.constraint(equalTo: signInOrSignUpSegmentedControl.widthAnchor, multiplier: 1 / CGFloat(signInOrSignUpSegmentedControl.numberOfSegments)).isActive = true
+        firstButton.titleLabel?.textColor = UIColor.orange
+        secondButton.titleLabel?.textColor = UIColor.orange
 
     }
     
-    @IBAction func signInOrSignUpSegmentedControlChanged(_ sender: UISegmentedControl) {
-        signInOrSignUpSegmentedControl.backgroundColor = .clear
-        signInOrSignUpSegmentedControl.tintColor = .clear
-    }
-    
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
