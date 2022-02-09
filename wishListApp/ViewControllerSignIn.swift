5ba89d58a4383ef8411db74b331dbbc40cfe4012
Avnish K <avnish.khandeshi24@d155.org>
2022-02-09 09:49:27 -0600

    set up sign in/up view controller

--- wishListApp/ViewControllerSignIn.swift
+++ wishListApp/ViewControllerSignIn.swift
@@ -9,8 +9,6 @@ import UIKit
 
 class ViewControllerSignIn: UIViewController {
     
-    @IBOutlet weak var segmentedControlStackView: UIStackView!
-    
     @IBOutlet weak var firstButton: UIButton!
     
     @IBOutlet weak var secondButton: UIButton!
@@ -20,12 +18,36 @@ class ViewControllerSignIn: UIViewController {
     
     @IBOutlet weak var secondImageView: UIImageView!
     
+    @IBOutlet weak var labelUnderHello: UILabel!
+    
+    
+    
+    @IBOutlet var firstImageViewTapGestureRecognizer: UITapGestureRecognizer!
+    
+    @IBOutlet var secondImageViewTapGestureRecognizer: UITapGestureRecognizer!
+    
+    
+    
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBOutlet weak var signInSignUpButton: UIButton!
+    
+    
+    
+    
     
     
     override func viewWillAppear(_ animated: Bool) {
         
         firstButton.titleLabel?.textColor = UIColor.orange
-        secondButton.titleLabel?.textColor = UIColor.orange
+        secondButton.titleLabel?.textColor = UIColor.lightGray
+        
+        firstImageView.backgroundColor = UIColor.orange
+        secondImageView.backgroundColor = UIColor.lightGray
 
     }
     
@@ -35,6 +57,78 @@ class ViewControllerSignIn: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(red: 0.9137, green: 0.9255, blue: 0.9686, alpha: 1)
+        
+        usernameTextField.layer.cornerRadius = 15
+        passwordTextField.layer.cornerRadius = 15
+        
+        signInSignUpButton.backgroundColor = UIColor.blue
+        signInSignUpButton.titleLabel?.textColor = UIColor.white
+        signInSignUpButton.layer.cornerRadius = 20
     }
+    
+    
+    @IBAction func firstButtonAction(_ sender: UIButton) {
+        
+        firstButton.titleLabel?.textColor = UIColor.orange
+        secondButton.titleLabel?.textColor = UIColor.lightGray
+        
+        firstImageView.backgroundColor = UIColor.orange
+        secondImageView.backgroundColor = UIColor.lightGray
+        
+        signInSignUpButton.titleLabel?.text = "Sign In"
+        
+        labelUnderHello.text = "Sign In to Your Account"
+        
+    }
+    
+    
+    @IBAction func secondButtonAction(_ sender: UIButton) {
+        
+        firstButton.titleLabel?.textColor = UIColor.lightGray
+        secondButton.titleLabel?.textColor = UIColor.orange
+        
+        firstImageView.backgroundColor = UIColor.lightGray
+        secondImageView.backgroundColor = UIColor.orange
+        
+        signInSignUpButton.titleLabel?.text = "Sign Up"
+        
+        labelUnderHello.text = "Sign Up to Create Your Account"
+        
+    }
+    
+    
+    @IBAction func firstImageViewTapGestureRecognizerAction(_ sender: UITapGestureRecognizer) {
+        
+        firstButton.titleLabel?.textColor = UIColor.orange
+        secondButton.titleLabel?.textColor = UIColor.lightGray
+        
+        firstImageView.backgroundColor = UIColor.orange
+        secondImageView.backgroundColor = UIColor.lightGray
+        
+        signInSignUpButton.titleLabel?.text = "Sign In"
+        
+        labelUnderHello.text = "Sign In to Your Account"
+        
+    }
+    
+    @IBAction func secondImageViewTapGestureRecognizer(_ sender: UITapGestureRecognizer) {
+        
+        firstButton.titleLabel?.textColor = UIColor.lightGray
+        secondButton.titleLabel?.textColor = UIColor.orange
+        
+        firstImageView.backgroundColor = UIColor.lightGray
+        secondImageView.backgroundColor = UIColor.orange
+        
+        signInSignUpButton.titleLabel?.text = "Sign Up"
+        
+        labelUnderHello.text = "Sign Up to Create Your Account"
+        
+    }
+    
+    
+    
+    
+    
     
 }
